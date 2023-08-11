b79f6bd8246fbe0f29a92a0082eab7c3636d8d46
WELLINGTON DA SILVA CAMPOS MOREIRA <1091392313025@fatec-srv.local>
2023-08-11 20:52:13 -0300

    aula_02

---
+++ Aula01/exemplo02.ts
@@ -0,0 +1,20 @@
+namespace exemplo2 {
+//VAR, LET, CONST
+let numero: number;
+numero = 5;
+
+let resultado: number;
+resultado = numero + numero;
+console.log("A soma entre " + numero + " com " + numero + " é igual a: " + resultado );
+
+resultado = numero - 4;
+console.log(`A subtração entre ${numero} com 4 é igual a ${resultado}`);
+
+resultado = numero * numero;
+console.log(`A multiplicação entre ${numero} * ${numero} é igual a ${resultado}`);
+
+resultado = numero / numero;
+console.log(`A divisão entre ${numero} / ${numero} é igual a ${resultado}`)
+
+
+}
